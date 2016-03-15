adfa927c404697031a3f2c6d4f7fef7e0ab7c3f1
William Blondel <william.blondel@lecole-ldlc.com>
2016-03-16 00:49:31 +0100

    Small fix

--- main.py
+++ main.py
@@ -5,26 +5,24 @@ import time
 def DemoNormalPrimes_CribleEratosthene(longueur, fichier):
     start_time1 = time.clock()
     primes = ulam.InitialisePremiers_CribleEratosthene(int(longueur))
-    #print(primes)
     sp = ulam.GenerateurSpirale(int(longueur), fichier, primes)
     sp.ColorisationImage()
     print("Méthode 1 :", time.clock() - start_time1, "seconds")
-    # choix = input("Afficher la spirale ? (O/N) ")
-    # if choix == "O":
-    #     ulam.show_spiral(longueur, primes)
+    choix = input("Afficher la spirale ? (O/N) ")
+    if choix == "O":
+        ulam.show_spiral(longueur, primes, symbol="")
 
 
 def DemoNormalPrimes_methode2(longueur, fichier):
     start_time2 = time.clock()
     primes = ulam.InitialisePremiers_methode2(longueur)
-    #print(primes[1:])
     sp = ulam.GenerateurSpirale(longueur, fichier, primes[1:])
     sp.ColorisationImage()
     print("Méthode 2 :", time.clock() - start_time2, "seconds")
 
     choix = input("Afficher la spirale ? (O/N) ")
     if choix == "O":
-        ulam.show_spiral(longueur, primes[1:])
+        ulam.show_spiral(longueur, primes[1:], symbol="")
 
 
 def main():
