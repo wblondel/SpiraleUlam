a8f7b83877febfe3c00798df337cedb88fb3d2d8
William Blondel <william.blondel@lecole-ldlc.com>
2016-03-16 01:07:40 +0100

    Added: license

--- main.py
+++ main.py
@@ -1,3 +1,23 @@
+# coding: utf8
+
+# SpiraleUlam  Copyright (C) 2016    William Gerald Blondel
+# william.blondel78@gmail.com
+# Last modified 16th March 2016 1.06am
+
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+
 import ulam
 import time
 
